55d6f2c1612c5b10c0e2465737881fb2104e1b9f
Sergey Lavrov <lavrov-sergey@yandex.ru>
2019-02-07 13:57:02 +0300

    Интерфейс и код связаны

--- PeerReview04/ViewController.m
+++ PeerReview04/ViewController.m
@@ -9,15 +9,21 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *startLocation;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationA;
+@property (weak, nonatomic) IBOutlet UILabel *distanceA;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationB;
+@property (weak, nonatomic) IBOutlet UILabel *distanceB;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocationC;
+@property (weak, nonatomic) IBOutlet UILabel *distanceC;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
 
 @end
